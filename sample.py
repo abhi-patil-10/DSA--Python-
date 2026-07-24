00078ac76066024db6431a6fd9b2c5512fd8d18c
Abhijeet Patil <abhigithub777@gmail.com>
2026-07-25 00:08:56 +0530

    practice work hacker rank problems

--- sample.py
+++ sample.py
@@ -1,15 +1,65 @@
-import numpy
 
-arr = [1 ,2, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 8, 4, 3, 1, 5, 6, 2]
 
-freq = {}
-low = 0
-for num in arr:
-    if num in freq:
-        freq[num] += 1
-    else:
-        low = num
-        freq[num] = 1
+# def subset(A,B):
+#     counter = 0
+#     for i in A:
+#         if i in B:
+#             counter += 1
         
-print(freq)
-print(low)
+#     if counter == len(A):
+#         return True
+#     else:
+#         return False
+        
+    
+# # A = set(map(int,input().split()))
+# # B = set(map(int,input().split()))
+# # print(A.issubset(B))
+
+# # 1 2 3 4 5 6 7 8 9 10 11 12 23 45 84 78
+# # 2
+# # 1 2 3 4 5
+# # 100 11 12
+# # A = (1,2,3,4,5,6,7,8,9,10,11,12,23,45,84,78)
+# # B = (1,2,3,4,5)
+# # C = (100, 11, 12)
+# # A = set(map(int,input().split()))
+# # no_of_set = int(input())
+# # for _ in range(no_of_set):
+# #     B = set(map(int,input().split()))
+# #     C = set(map(int,input().split()))
+# #     if A.issuperset(B) and A.issuperset(C):
+# #         return True
+# #     else:
+# #         return False
+
+# no_of_testcases = int(input())
+# for _ in range(no_of_testcases):
+#     A,B = input().split()
+#     if not A.isdigit():
+#         non_int = A
+#     else:
+#         non_int = B
+    
+#     try:
+#         print(int(A)//int(B))
+#     except ZeroDivisionError:
+#         print("Error Code: integer division or modulo by zero")
+#     except ValueError:
+#         print("Error Code: invalid literal for int() with base 10: '{}'"
+#         .format(non_int))
+
+
+arr = list(input().split(" "))
+substring = input()
+counter = 0
+
+for word in arr:
+    if word == substring:
+        counter += 1
+
+print(counter)
+
+    
+
+# print(arr)
