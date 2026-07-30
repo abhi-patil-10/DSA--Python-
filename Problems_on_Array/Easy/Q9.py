c6feb54f924c860a25e1b04230cdc88623b3e88f
Abhijeet Patil <abhigithub777@gmail.com>
2026-07-31 00:01:08 +0530

    intersection of two array of array(easy) completed

--- Problems_on_Array/Easy/Q9.py
+++ Problems_on_Array/Easy/Q9.py
@@ -1,13 +1,32 @@
 #Find union of two sorted arrays
 
+#brute force 
+# arr1 = [1,2,2,2,3,4,5,5,5]
+# arr2 = [2,3,4,7,8,8]
+# union = set()
+# for i in range(len(arr1)):
+#     union.add(arr1[i])
+    
+# for i in range(len(arr2)):
+#     union.add(arr2[i])
+    
+# print(list(union))
 
-arr1 = [1,2,2,2,3,4,5,5,5]
-arr2 = [2,3,4,7,8,8]
-union = set()
-for i in range(len(arr1)):
-    union.add(arr1[i])
-    
-for i in range(len(arr2)):
-    union.add(arr2[i])
-    
-print(list(union))
+#find the intersection of two sorted arrays
+arr1 = [1,2,3,4,4,4,5,6]
+arr2 = [3,4,4,5,5,6]
+i = 0
+j = 0
+intersection = []
+while i<len(arr1) and j < len(arr2):
+    if arr1[i] < arr2[j]:
+        i+=1
+    elif arr1[i] > arr2[j]:
+        j+=1
+    else:
+        intersection.append(arr1[i])
+        i+=1
+        j+=1
+        
+print(intersection)
+        
